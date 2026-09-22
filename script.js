fa483477a1f72a4fd1254c80df526849a3731595
Rwuijster Dev <wuijster.rick@gmail.com>
2026-09-22 11:43:22 +0200

    added accessibility to navbar

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-const navbar = document.getElementById("navbar")
+const navbar = document.getElementById("navbar");
+const openButton = document.getElementById("open-sidebar-button");
+
+const media = window.matchMedia("(width < 800px)")
+
+media.addEventListener('change', (e) => updateNavbar(e))
+
+function updateNavbar(e) {
+	const isMobile = e.matches
+	console.log(isMobile)
+	if(isMobile) {
+		navbar.setAttribute('inert', '')
+	}
+	else {
+		//On Desktop device
+		navbar.removeAttribute('inert')
+	}
+}
 
 function openSidebar() {
 	navbar.classList.add("show")
+	openButton.setAttribute("aria-expanded", "true")
+	navbar.removeAttribute("inert")	
 }
 
 function closeSidebar() {
 	navbar.classList.remove("show")
+	openButton.setAttribute("aria-expanded", "false")
+	navbar.setAttribute("inert", "")
 }
+
+
+//Close sidebar when clicking a link
+const navLinks = document.querySelectorAll('nav a')
+nav.Links.forEach(link => {
+	link.addEventListener('click', () => {
+		closeSidebar()
+	})
+})
+
+updateNavbar(media)
